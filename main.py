7d1305971ba9b409ddbe1547dd6b6637af01892a
romulo <romulo.allc@gmail.com>
2026-03-05 22:58:10 -0300

    groups

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
     player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2), PLAYER_RADIUS)
 
@@ -24,8 +28,10 @@ def main():
                 return
         
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
+        for drew in drawable:
+            drew.draw(screen)
+
         pygame.display.flip()
 
         dt = (clock.tick(60))/1000
